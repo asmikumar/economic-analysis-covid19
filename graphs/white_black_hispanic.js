04b11f52f0001e083c2d1e32d1114eed2ba7e514
Reshmi2121 <43017750+Reshmi2121@users.noreply.github.com>
2020-08-03 13:49:09 -0400

    Added graphs and aesthetics

--- graphs/white_black_hispanic.js
+++ graphs/white_black_hispanic.js
@@ -1,29 +1,51 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// Area Chart Example
 var line_chart = document.getElementById("white_black_hispanic");
 
 var dataFirst = {
-    label: "White vs. Black % Gap",
-    data: [0, 59, 75, 20, 20, 55, 40],
+    label: "Median White Pay Per Hour",
+    data: [17.94, 17.51, 17.44, 17.53, 17.48, 17.71, 17.45, 17.39, 16.98, 17.25,
+          17.11, 17.32, 17.27, 17.78, 17.74, 17.83, 17.78, 17.82, 18.09, 17.83,
+          17.57, 17.36, 17.64, 17.81, 18.13, 18.77, 19.06, 19.28, 19.85, 20.04,
+          20.17, 20.26, 19.77, 19.81, 19.91, 20.03, 20.41, 20.26, 19.86, 19.98,
+          19.86, 19.86, 20.42, 20.59, 20.86, 20.94, 21.32],
     lineTension: 0,
     fill: false,
     borderColor: 'red'
   };
 
 var dataSecond = {
-    label: "White vs. Hispanic % Gap",
-    data: [20, 15, 60, 60, 65, 30, 70],
+    label: "Median Black Pay Per Hour",
+    data: [13.95, 14.04, 14.11, 14.2, 14.18, 14.15, 14.58, 14.38, 14.01, 13.91,
+          13.98, 13.95, 13.93, 14.23, 14.26, 14.55, 14.15, 14.23, 14.09, 14.18,
+          14.08, 13.86, 13.97, 13.93, 14.24, 14.95, 15.32, 15.28, 15.36, 15.77,
+          16.09, 16.22, 15.61, 15.73, 15.46, 15.41, 16.05, 15.96, 15.46, 15.06,
+          15.39, 15.07, 15.19, 15.86, 15.59, 15.59, 16.12],
     lineTension: 0,
     fill: false,
     borderColor: 'blue'
   };
 
+  var dataThird = {
+      label: "Median Hispanic Pay Per Hour",
+      data: [14.3, 14.17, 14.09, 14.16, 14.11, 14.1, 14.39, 14.23, 13.63, 13.75,
+            13.62, 13.77, 13.7, 13.69, 13.68, 13.52, 13.27, 13.27, 12.95, 13.06,
+            12.95, 12.65, 12.47, 12.82, 12.82, 13.23, 13.43, 13.44, 14.08, 14.18,
+            14.01, 13.7, 13.72, 14.04, 14.32, 14.34, 14.44, 14.14, 13.77, 13.75,
+            13.74, 14.01, 14.42, 14.99, 15.54, 15.31, 15.89],
+      lineTension: 0,
+      fill: false,
+      borderColor: 'green'
+    };
+
 var data = {
-  labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
-  datasets: [dataFirst, dataSecond]
+  labels: ["1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982",
+          "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992",
+          "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002",
+          "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012",
+          "2013", "2014", "2015", "2016", "2017", "2018", "2019"],
+  datasets: [dataFirst, dataSecond, dataThird]
 };
 
 var chartOptions = {
