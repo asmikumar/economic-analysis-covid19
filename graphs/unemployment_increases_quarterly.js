49260b211f8e58be369edfe5e13a8b2a56c2087a
asmikumar <asmikumar.ak@gmail.com>
2020-08-07 00:11:01 -0400

    Adding quarterly unemployment trend graphs

---
+++ graphs/unemployment_increases_quarterly.js
@@ -0,0 +1,47 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Bar Chart Example
+var ctx = document.getElementById("unemployment_increases_quarterly");
+var unemployment_increases_quarterly = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["White", "Black", "Hispanic", "Women"],
+    
+    datasets: [
+        {
+            backgroundColor: ["rgba(0,153,255,1)", "rgba(102,102,204,1)", "rgba(255,204,0,1)", "rgba(255,51,0,1)"],
+            data: [281.3,191.1,304.8,302.9]
+        }
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          maxTicksLimit: 20
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
